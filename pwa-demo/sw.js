2e5e72b85d6a00972ca29d120cc1aadee4de1dcc
luobo <luobochuanqi@qq.com>
2026-09-05 14:42:05 +0800

    Add minimal installable Hello PWA demo

---
+++ pwa-demo/sw.js
@@ -0,0 +1,19 @@
+// Service Worker：预缓存应用外壳，离线可用
+const CACHE = 'hello-pwa-v1';
+const ASSETS = ['./', './index.html', './manifest.webmanifest', './icon-192.png', './icon-512.png'];
+
+self.addEventListener('install', (e) => {
+  e.waitUntil(caches.open(CACHE).then((c) => c.addAll(ASSETS)).then(() => self.skipWaiting()));
+});
+
+self.addEventListener('activate', (e) => {
+  e.waitUntil(caches.keys().then((keys) =>
+    Promise.all(keys.filter((k) => k !== CACHE).map((k) => caches.delete(k)))
+  ).then(() => self.clients.claim()));
+});
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((hit) => hit || fetch(e.request))
+  );
+});
